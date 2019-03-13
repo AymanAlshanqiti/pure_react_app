54ada0eab0e01012c57f1be3e3295e5a8d2310f4
Ayman Alshanqiti <ayman.alshanqiti@gmail.com>
2019-03-13 20:59:54 +0300

    Events and State examples

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    counter: 0
+  };
+
+  clickHandler = name => alert(`Heloooooooo ${name}`);
+
+  goUp = () => {
+    const newValue = this.state.counter + 1;
+    this.setState({ counter: newValue });
+  };
+
+  goDown = () => {
+    const newValue = this.state.counter - 1;
+    this.setState({ counter: newValue });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <br />
+        <button onClick={() => this.clickHandler("Ayman")}> Click Me </button>
+        <br />
+        <br />
+        <br />
+
+        <button onClick={this.goUp}>+</button>
+        <p>{this.state.counter}</p>
+        <button onClick={this.goDown}>-</button>
       </div>
     );
   }
